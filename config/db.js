ea581c1f21383abbe895ae31d74763c16a8606d1
patzkatigbak <patzkatigbak@gmail.com>
2023-02-16 20:47:33 -0500

    added a file for local service setup

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports ={
-  'db': process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/money_mindset'
+ // 'db': process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/money_mindset'
+  'db': process.env.MONGODB_URI || 'mongodb+srv://eric:qq123@cluster0.x4l232u.mongodb.net/money-mindset?retryWrites=true&w=majority'
 };
