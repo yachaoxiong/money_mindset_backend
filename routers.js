bbc9f49539b74c5dfb48831f144caba1b3f81410
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2023-03-30 17:38:14 -0400

    add budget

--- routers.js
+++ routers.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth');
 const billRoutes = require('./routes/bill');
-
+const budgetRoutes = require('./routes/budget');
 app.use('/auth', authRoutes);
 app.use('/bill', billRoutes);
+app.use('/budget', budgetRoutes);
 
 module.exports = app;
